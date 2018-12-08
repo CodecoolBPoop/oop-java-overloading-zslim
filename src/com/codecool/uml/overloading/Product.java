7a16f471966c9bf0955fe4b999ae43c5357725d5
Zsofia Limbek <zsofia.limbek@gmail.com>
2018-12-08 12:37:25 +0100

    created getters & setters in Product

--- src/com/codecool/uml/overloading/Product.java
+++ src/com/codecool/uml/overloading/Product.java
@@ -1,5 +1,6 @@
 package com.codecool.uml.overloading;
 
+import java.util.ArrayList;
 import java.util.Currency;
 import java.util.List;
 
@@ -11,5 +12,62 @@ public class Product {
     private Currency defaultCurrency;
     private ProductCategory productCategory;
     private Supplier supplier;
-    private List<Product> productList;
+    private static List<Product> productList = new ArrayList<>();
+
+    public Product() {
+        productList.add(this);
+        id = productList.size();
+    }
+
+    public Product(String name, float defaultPrice, Currency defaultCurrency) {
+        productList.add(this);
+        id = productList.size();
+        this.name = name;
+        this.defaultPrice = defaultPrice;
+        this.defaultCurrency = defaultCurrency;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getDefaultPrice() {
+        return defaultPrice;
+    }
+
+    public void setDefaultPrice(float defaultPrice) {
+        this.defaultPrice = defaultPrice;
+    }
+
+    public Currency getDefaultCurrency() {
+        return defaultCurrency;
+    }
+
+    public void setDefaultCurrency(Currency defaultCurrency) {
+        this.defaultCurrency = defaultCurrency;
+    }
+
+    public ProductCategory getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(ProductCategory productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
 }
